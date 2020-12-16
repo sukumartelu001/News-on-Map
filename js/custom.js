8c0dd0f66e92a338596a59ebf6e2f8d61246f68a
sukumartelu001 <76096442+sukumartelu001@users.noreply.github.com>
2020-12-16 17:53:02 +0600

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -15,7 +15,7 @@ Array.prototype.unique =
   };
 
 //Initialize the maps
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
